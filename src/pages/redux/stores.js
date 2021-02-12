cb911b6980bf0fa2be4bab038607d553adc2c0d4
FrancisMasangcay <francis.masangcay@yahoo.com>
2021-02-12 11:53:38 -0600

    removed redux dev tools from store compose

--- src/pages/redux/stores.js
+++ src/pages/redux/stores.js
@@ -18,8 +18,9 @@ const store = createStore(
   reducers,
   initialState,
   compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middleware)
+    //uncomment for dev tools
+    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
